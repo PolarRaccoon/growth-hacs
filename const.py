1ac76cb9b6b899ca56d32daaf4c9bbeb59a6b684
PolarRaccoon <beltain@me.com>
2026-02-22 21:18:43 +0100

    Create const.py

---
+++ const.py
@@ -0,0 +1,34 @@
+"""Constants for the GROWTH integration."""
+
+DOMAIN = "growth"
+
+CONF_NAME = "name"
+CONF_BIRTH_DATE = "birth_date"
+CONF_GENDER = "gender"
+CONF_WEIGHT = "weight"
+CONF_HEIGHT = "height"
+CONF_CHEST = "chest"
+CONF_WAIST = "waist"
+CONF_HIP = "hip"
+CONF_FOOT_LENGTH = "foot_length"
+
+GENDER_MALE = "male"
+GENDER_FEMALE = "female"
+
+ATTR_AGE = "age"
+ATTR_BMI = "bmi"
+ATTR_BMI_CATEGORY = "bmi_category"
+ATTR_BODY_SHAPE = "body_shape"
+ATTR_EU_TOP = "eu_top"
+ATTR_EU_BOTTOM = "eu_bottom"
+ATTR_EU_SHOE = "eu_shoe"
+ATTR_CHEST = "chest_cm"
+ATTR_WAIST = "waist_cm"
+ATTR_HIP = "hip_cm"
+ATTR_FOOT_LENGTH = "foot_length_cm"
+ATTR_HEIGHT = "height_cm"
+ATTR_WEIGHT = "weight_kg"
+ATTR_GENDER = "gender"
+ATTR_BIRTH_DATE = "birth_date"
+
+UPDATE_INTERVAL = 3600  # seconds (1 hour) - catches daily age changes
